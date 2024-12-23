4d72e60c04bcc407dcd41b1d42e50283738f6f14
jeldikk <jeldi.kamal2013@gmail.com>
2024-12-23 18:29:48 +0530

    add cors middleware, change register response

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,7 @@
 require("express-async-errors");
 const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
 const cookieSession = require("cookie-session");
 const healthRouter = require("./routes/health.router");
 const clustersRouter = require("./routes/clusters.router");
@@ -9,6 +11,8 @@ const NotFoundError = require("./errors/not-found.error");
 
 const app = express();
 
+app.use(cors())
+app.use(morgan('combined'))
 app.use(express.json({limit: '10mb'}));
 app.use(express.urlencoded({extended: true, limit: '5mb'}))
 app.use(
@@ -19,6 +23,7 @@ app.use(
         maxAge: 2 * 60 * 60 * 1000
     })
 )
+
 // add your routers below
 app.use('/api/health', healthRouter);
 app.use("/api/v1/auth", authRouter);
